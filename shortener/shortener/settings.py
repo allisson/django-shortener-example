d037c913ed18e0a7b24865b7f4f5aaf68df2cca3
Allisson Azevedo <allisson@gmail.com>
2013-05-11 13:01:13 -0300

    Adicionando o gunicorn na lista de apps.

--- shortener/shortener/settings.py
+++ shortener/shortener/settings.py
@@ -128,6 +128,8 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
+    # external apps
+    'gunicorn',
     # local apps
     'accounts',
     'shorturl',
